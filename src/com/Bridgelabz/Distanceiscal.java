a82854cc966b256f150b2332013e9b1bfe7a5d93
Divakar <divakar39312@gmail.com>
2022-08-04 21:57:16 +0530

    [Divakar]: UC2 file added

--- src/com/Bridgelabz/Distanceiscal.java
+++ src/com/Bridgelabz/Distanceiscal.java
@@ -6,14 +6,24 @@ public class Distanceiscal {
 		
 
 		/*
-		 * two point distan is cal
+		 * equality of two line
 		 */
-		int x1=3;
+		
+		int x1=8;
 		int x2=7;
 		int y1=7;
-		int y2=2;
-		double dist=Math.floor(Math.sqrt(Math.pow(x2-x1, 2)+Math.pow(y2-y1, 2)));
-		System.out.println(dist);
-	}
-
+		int y2=7;
+		int x3=6;
+		int x4=6;
+		int y3=7;
+		int y4=6;
+		
+		double distof1 =(Math.sqrt(Math.pow(x2-x1, 2)+Math.pow(y2-y1,2)));
+		double distof2 =(Math.sqrt(Math.pow(x4-x3, 2)+Math.pow(y4-y3,2)));
+		System.out.println("length of first line"+distof1);
+		System.out.println("length of first line"+distof2);
+		Integer a1=new Integer((int)distof1);
+		Integer a2=new Integer((int)distof2);
+		System.out.println("line are equal"+a1.equals(a2));
+}
 }
