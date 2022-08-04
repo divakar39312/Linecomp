953b89441f4f9e54344a149766304709518c2217
Divakar <divakar39312@gmail.com>
2022-08-04 21:40:03 +0530

    [Divakar]:file added

---
+++ src/com/Bridgelabz/Distanceiscal.java
@@ -0,0 +1,19 @@
+package com.Bridgelabz;
+
+public class Distanceiscal {
+
+	public static void main(String[] args) {
+		
+
+		/*
+		 * two point distan is cal
+		 */
+		int x1=3;
+		int x2=7;
+		int y1=7;
+		int y2=2;
+		double dist=Math.floor(Math.sqrt(Math.pow(x2-x1, 2)+Math.pow(y2-y1, 2)));
+		System.out.println(dist);
+	}
+
+}
